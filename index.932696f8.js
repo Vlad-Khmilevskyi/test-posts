9d6d587b5604e9b262474b3cfc174fa901132dc4
Vlad-Khmilevskyi <103739251+Vlad-Khmilevskyi@users.noreply.github.com>
2023-07-20 12:27:54 +0000

    Deploying to gh-pages from @ Vlad-Khmilevskyi/test-posts@9421814a8de0c0a13e41daff18b2120e5b34a7e1 🚀

--- index.932696f8.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector(".btn"),n=document.querySelector(".user-list");t.addEventListener("click",(()=>{fetch("https://rest-api-production-fc73.up.railway.app/api/v1/posts").then((t=>{if(!t.ok)throw new Error(t.status);return t.json()})).then((t=>function(t){const e=t.map((t=>`\n          <li>\n            <p><b>Name</b>: ${t.title}</p>\n            <p><b>Email</b>: ${t.description}</p>\n            <p><b>Company</b>: ${t.text}</p>\n          </li>\n      `)).join("");n.innerHTML=e}(t))).catch((t=>console.log(t)))}));
-//# sourceMappingURL=index.932696f8.js.map
